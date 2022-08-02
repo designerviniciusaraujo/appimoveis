9751371393f18b4cc59f2f8dcb5e24e14a748771
Vinicius Araujo <designerviniciusaraujo@gmail.com>
2022-08-02 14:06:01 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,527 @@
+ 
+
+
+  const allProds = [{  
+     categoria :"Cervejas",
+    "itens":[
+      { 
+        id:'0',
+        name:'Brahma',
+        description:'',
+        price:4.00,
+        img:'produtos/brahma.png',
+        quantidade: 0,
+      },{ 
+        id:'1',
+        name:'Itaipava',
+        description:'',
+        price:3.5,
+        img:'produtos/itaipava.png',
+        quantidade: 0,
+      },{ 
+        id:'2',
+        name:'Original',
+        description:'',
+        price:6.5,
+        img:'produtos/original.png',
+        quantidade: 0,
+      },{ 
+        id:'3',
+        name:'Heineken',
+        description:'',
+        price:6.50,
+        img:'produtos/heineken.png',
+        quantidade: 0,
+      },{ 
+        id:'4', 
+        name:'Império Cerveja Lata ', 
+        description:'',
+        price:4.50,
+        img:'produtos/imperio.png',
+        quantidade: 0,
+      },{ 
+        id:'6', 
+        name:'Haineken Long Neck', 
+        description:'',
+        price:8.50,
+        img:'produtos/long_hei.png',
+        quantidade: 0,
+      },{ 
+        id:'7',
+        name:'Long Budweiser',
+        description:'',
+        price:7.50,
+        img:'produtos/long_bud.png',
+        quantidade: 0,
+      },{ 
+        id:'8',
+        name:'Stella',
+        description:'',
+        price:8.50,
+        img:'produtos/long_stella.png',
+        quantidade: 0,
+      },{ 
+        id:'9',
+        name:'Corona',
+        description:'',
+        price:9.50,
+        img:'produtos/long_corona.png',
+        quantidade: 0,
+      },{ 
+        id:'26',
+        name:'Itaipava 0%',
+        description:'',  
+        price:5.00,
+        img:'produtos/itaipava0%.png',
+        quantidade: 0,
+      },{ 
+        id:'27',
+        name:'Brahma 0%',
+        description:'',
+        price:6.00,
+        img:'produtos/brahma0%.png',
+        quantidade: 0,
+      },{ 
+        id:'4',
+        name:'Império unidade',
+        description:'',
+        price:4.50,
+        img:'produtos/imperio.png',
+        quantidade: 0,
+      },{ 
+        id:'62',
+        name:'Brahma Duplo Malte',
+        description:'',
+        price:5.00,
+        img:'produtos/brahma_puro_malte.png',
+        quantidade: 0,
+      },{ 
+        id:'39',
+        name:'Spaten Unidade',
+        description:'',
+        price:6.00,
+        img:'produtos/spaten.png',
+        quantidade: 0,
+      },{ 
+        id:'29',
+        name:'Itaipava Malzbier',
+        description:'',
+        price:6.50,
+        img:'produtos/itaipava_malzbier.png',
+        quantidade: 0,
+      },{ 
+        id:'61',
+        name:'Petra',
+        description:'',
+        price:5.50,
+        img:'produtos/petra.png',
+        quantidade: 0,
+      },{ 
+        id:'21',
+        name:'Budweiser Lata',
+        description:'',
+        price:5.00,
+        img:'produtos/budweiser_lata.png',
+        quantidade: 0,
+      }
+  ]
+  },
+  {  
+    categoria:"Whisky",
+    "itens":[
+      { 
+        id:'25',
+        name:'Black Label',
+        description:'',  
+        price:140.00,
+        img:'produtos/blacklabel.png',
+        quantidade: 0,
+      },{ 
+        id:'20',
+        name:'Red',
+        description:'',
+        price:100.00,
+        img:'produtos/redlabel.png',
+        quantidade: 0,
+      },{ 
+        id:'44',
+        name:'Famous One whisky',
+        description:'',
+        price:25.00,
+        img:'produtos/famous_one_whisky.png',
+        quantidade: 0,
+      },{ 
+        id:'49',
+        name:'Ballantines',
+        description:'',
+        price:99.00,
+        img:'produtos/ballantines.png',
+        quantidade: 0,
+      },{ 
+        id:'55',
+        name:'Old star',
+        description:'',
+        price:20.00,
+        img:'produtos/oldstar.png',
+        quantidade: 0,
+      },{ 
+        id:'56',
+        name:'Passport',
+        description:'',
+        price:55.00,
+        img:'produtos/passport.png',
+        quantidade: 0,
+      },{ 
+        id:'57',
+        name:'Black stone',
+        description:'',
+        price:35.00,
+        img:'produtos/blackstone.png',
+        quantidade: 0,
+      },{ 
+        id:'45',
+        name:'Master Gold',
+        description:'',
+        price:25.00,
+        img:'produtos/mastergold.png',
+        quantidade: 0,
+      },{ 
+        id:'59',
+        name:'Jack Daniels Whisky',
+        description:'',
+        price:120.00,
+        img:'produtos/jack.png',
+        quantidade: 0,
+      },{ 
+        id:'47',
+        name:'Contini',
+        description:'',
+        price:38.00,
+        img:'produtos/contini.png',
+        quantidade: 0,
+      },{ 
+        id:'48',
+        name:'Gold Label',
+        description:'',
+        price:190.00,
+        img:'produtos/goldlabel.png',
+        quantidade: 0,
+      }
+  ]  
+ },
+ {  
+  categoria:"Fardo",
+  "itens":[
+    { 
+      id:'31',
+      name:'Fardo Skol',
+      description:'',
+      price: 36.00,
+      img:'produtos/fardo_skol.png',
+      quantidade: 0,
+    },{ 
+      id:'32',
+      name:'Fardo Brahma',
+      description:'',
+      price:38.00,
+      img:'produtos/fardo_Brahma.png',
+      quantidade: 0,
+    },{ 
+      id:'33',
+      name:'Fardo Heineken',
+      description:'',
+      price:59.00,
+      img:'produtos/fardo_heineken.png',
+      quantidade: 0,
+    },{ 
+      id:'34',
+      name:'Fardo Original',
+      description:'',
+      price:54.00,
+      img:'produtos/fardo_original.png',
+      quantidade: 0,
+    },{ 
+      id:'35',
+      name:'Fardo Budweiser',
+      description:'',
+      price:28.00,
+      img:'produtos/fardo_bud.png',
+      quantidade: 0,
+    },{ 
+      id:'36',
+      name:'Fardo Petra',
+      description:'',
+      price:38.00,
+      img:'produtos/fardo_petra.png',
+      quantidade: 0,
+    },{ 
+      id:'37',
+      name:'Fardo Eisenbahn',
+      description:'',
+      price:65.00,
+      img:'produtos/fardo_eisenbahn.png',
+      quantidade: 0,
+    },{ 
+      id:'38',
+      name:'Fardo Spaten',
+      description:'',
+      price:59.00,
+      img:'produtos/fardo_spaten.png',
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Vinhos",
+  "itens":[
+    { 
+      id:'12',
+      name:'Vinho Santome',
+      description:'',
+      price:22.00,
+      img:'produtos/vinho_santome.png',
+      quantidade: 0,
+    },{ 
+      id:'15',
+      name:'Vinho',
+      description:'',
+      price:8.00, 
+      img:'produtos/sem-foto.png', 
+      quantidade: 0,
+    },{ 
+      id:'19',
+      name:'Vinho Draft',
+      description:'',
+      price:14.00,  
+      img:'produtos/sem-foto.png',
+
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Gin",
+  "itens":[
+    { 
+      id:'65',
+      name:'Gin',
+      description:'',
+      price:130.00,
+      img:'produtos/gin.png',
+      quantidade: 0,
+    },{ 
+      id:'50',
+      name:'Gin Tanqueray',
+      description:'',
+      price:199.00,
+      img:'produtos/sem-foto.png',
+      quantidade: 0,
+    },{ 
+      id:'51',
+      name:'Gin Rose',
+      description:'',
+      price:28.00,
+      img:'produtos/sem-foto.png',
+      quantidade: 0,
+    }
+]  
+},
+{  
+  categoria:"Vodka",
+  "itens":[
+    { 
+      id:'58',
+      name:'Askov',
+      description:'',
+      price:18.50,
+      img:'produtos/sem-foto.png',
+      quantidade: 0,
+    }
+]  
+},
+{  
+  categoria:"Destilados",
+  "itens":[
+    { 
+      id:'58',
+      name:'Askov',
+      description:'',
+      price:18.50,
+      img:'produtos/askov.png',
+      quantidade: 0,
+    },{ 
+      id:'52',
+      name:'Dreher',
+      description:'',
+      price:21.00,
+      img:'produtos/dreher.png',
+      quantidade: 0,
+    },{ 
+      id:'53',
+      name:'51 Pinga',
+      description:'',
+      price:18.00,
+      img:'produtos/51.png',
+      quantidade: 0,
+    },{ 
+      id:'54',
+      name:'Pitu Pinga',
+      description:'',
+      price:25.00,
+      img:'produtos/pinga_pitu.png',
+      quantidade: 0,
+    },{ 
+      id:'42',
+      name:'Jurupinga',
+      description:'',
+      price:35.00,
+      img:'produtos/jurupinga.png',
+      quantidade: 0,
+    },{ 
+      id:'43',
+      name:'Velho Barreiro',
+      description:'',
+      price:18.00,
+      img:'produtos/velho_barreiro.png',
+      quantidade: 0,
+    },{ 
+      id:'30',
+      name:'Sminorf Lata',
+      description:'',
+      price:9.50,
+      img:'produtos/ice_lata.png',
+      quantidade: 0,
+    },{ 
+      id:'22',
+      name:'Sminorf Ice',
+      description:'',
+      price:13.00,
+      img:'produtos/ice.png',
+      quantidade: 0,
+    },{ 
+      id:'23',
+      name:'Corote', 
+      description:'',  
+      price:5.50,
+      img:'produtos/corote.png',
+      quantidade: 0,
+    },{ 
+      id:'59',
+      name:'Sminorf Sabor',
+      description:'',
+      price:58.00,
+      img:'produtos/sabor.png',
+      quantidade: 0,
+    },{ 
+      id:'17',
+      name:'Ice',
+      description:'',
+      price:7.50,
+      img:'produtos/ice.png',
+      quantidade: 0,
+    }
+]  
+},
+{  
+  categoria:"Energético",
+  "itens":[
+    { 
+      id:'10',
+      name:'Red Bull',
+      description:'',
+      price:12.00,
+      img:'produtos/redbull.png',
+      quantidade: 0,
+    },{ 
+      id:'11',
+      name:'Monster',
+      description:'',
+      price:12.50,
+      img:'produtos/monster.png',
+      quantidade: 0,
+    }
+]  
+},
+{  
+  categoria:"Refrigerante",
+  "itens":[
+    { 
+      id:'14',
+      name:'Refri Lata',
+      description:'',
+      price:5.00,
+      img:'produtos/coca_lata.png',
+      quantidade: 0,
+    },{ 
+      id:'13',
+      name:'Coca',
+      description:'',
+      price:12.00,
+      img:'produtos/coca_2l.png',
+      quantidade: 0,
+    },{ 
+      id:'5',
+      name:'Guaraná 1L',
+      description:'',
+      price:10.00,
+      img:'produtos/guarana1l.png',
+      quantidade: 0,
+    },{ 
+      id:'18',
+      name:'Tônica',
+      description:'',
+      price:5.00,
+      img:'produtos/tonica.png',
+      quantidade: 0,
+    },{ 
+      id:'28',
+      name:'Guaraná Lata',
+      description:'',
+      price:5.00,
+      img:'produtos/guarana_lata.png',
+      quantidade: 0,
+    }
+]  
+},{  
+  categoria:"Mais",
+  "itens":[
+    { 
+      id:'60',
+      name:'Água 1,5L',
+      description:'',
+      price:5.00,
+      img:'produtos/agua_1,5l.png',
+      quantidade: 0,
+    },{ 
+      id:'16',
+      name:'Gelo coco',
+      description:'',
+      price:4.00,
+      img:'produtos/gelo_coco.png',
+      quantidade: 0,
+    },{ 
+      id:'24',
+      name:'Halls',
+      description:'',   
+      price:3.00,
+      img:'produtos/halls.png',
+      quantidade: 0,
+    },{ 
+      id:'63',
+      name:'Fofura',
+      description:'',
+      price:4.00,
+      img:'produtos/fofura.png',
+      quantidade: 0,
+    },{ 
+      id:'64',
+      name:'Torcida',
+      description:'',
+      price:3.00,
+      img:'produtos/torcida.png',
+      quantidade: 0,
+    }
+]  
+}]
+
+
