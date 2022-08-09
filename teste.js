19c1d917096e742e3bb987b25748286463b278f2
Vinicius Araujo <designerviniciusaraujo@gmail.com>
2022-08-09 13:27:22 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -8,14 +8,14 @@
         id:'0',
         name:'Terreno',
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ',
-        price:45.0000,
+        price:450000, 
         img:'home.png',
         quantidade: 0,
       },{ 
         id:'1',
         name:'Terreno', 
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ',
-        price:3.500,
+        price:450000, 
         img:'home.png',
         quantidade: 0,
       } 
@@ -28,14 +28,14 @@
         id:'2',
         name:'Casa',
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ',
-        price:140.00,
+        price:450000, 
         img:'home.png',
         quantidade: 0,
       },{ 
         id:'3',
         name:'Casa',
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ',
-        price:100.00,
+        price:450000, 
         img:'home.png',
         quantidade: 0,
       } 
